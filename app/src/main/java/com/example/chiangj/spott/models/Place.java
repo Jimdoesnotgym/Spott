d181b501bc3e6c08412f1bbf3352ed6f57810300
Jim Chiang <Jim.Chiang@tomtom.com>
2017-07-31 16:17:01 +0800

    added retrofit, rxjava, rxandroid libraries, constructor for Place model

--- app/src/main/java/com/example/chiangj/spott/models/Place.java
+++ app/src/main/java/com/example/chiangj/spott/models/Place.java
@@ -6,4 +6,11 @@ public class Place {
     private String[] mTypes;
     private Object[] mPlacePhotos;
     private Object[] mAddressComponents;
+
+    public Place(String name, String[] types, Object[] placePhotos, Object[] addressComponents){
+        mName = name;
+        mTypes = types;
+        mPlacePhotos = placePhotos;
+        mAddressComponents = addressComponents;
+    }
 }
