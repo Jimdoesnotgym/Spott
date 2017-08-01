3d736366989638a374c31127fc91df2933a06423
Jim Chiang <Jim.Chiang@tomtom.com>
2017-08-01 15:20:55 +0800

    add splash screen but animation lags when switching activities

---
+++ app/src/main/java/com/example/chiangj/spott/activities/SplashScreenActivity.java
@@ -0,0 +1,39 @@
+package com.example.chiangj.spott.activities;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+import com.example.chiangj.spott.R;
+
+public class SplashScreenActivity extends Activity {
+
+    private static final int SPLASH_REQUEST_CODE = 1;
+
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashTransitionIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(splashTransitionIntent);
+            }
+        }, 3000);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
+}
